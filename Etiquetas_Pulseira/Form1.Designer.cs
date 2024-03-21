7821097e076ed1b0bd9eb3d3289d34c7dbb6e796
Henrique Lustosa Ribeiro Faria <h013026@rede.sp>
2024-03-21 12:29:18 -0300

    Projeto finalizado da etiqueta de pulseira da Quimioterapia

---
+++ Etiquetas_Pulseira/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Etiquetas_Nova_Versao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.btImprimir = new System.Windows.Forms.Button();
+            this.txbRh = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // btImprimir
+            // 
+            this.btImprimir.Location = new System.Drawing.Point(259, 331);
+            this.btImprimir.Name = "btImprimir";
+            this.btImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btImprimir.TabIndex = 0;
+            this.btImprimir.Text = "Imprimir";
+            this.btImprimir.UseVisualStyleBackColor = true;
+            this.btImprimir.Click += new System.EventHandler(this.btImprimir_Click);
+            // 
+            // txbRh
+            // 
+            this.txbRh.Location = new System.Drawing.Point(237, 117);
+            this.txbRh.Name = "txbRh";
+            this.txbRh.Size = new System.Drawing.Size(116, 20);
+            this.txbRh.TabIndex = 3;
+            this.txbRh.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbRh_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Informe RH";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(273, 382);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(254, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Pulseira";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Versão 1.0 (06/23)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 391);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbRh);
+            this.Controls.Add(this.btImprimir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Etiquetas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button btImprimir;
+        private System.Windows.Forms.TextBox txbRh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Etiquetas_Pulseira/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Drawing.Printing;
+using GenCode128;
+namespace Etiquetas_Nova_Versao
+{
+    public partial class Form1 : Form
+    {
+       
+
+        int status;
+        Paciente paciente;
+       // Internacao internacao;
+      //  List<Internacao> internacoes;
+        string error;
+        //string Andar ="";
+        ///string Quarto ="";
+        //string Leito ="";
+       // string Clinica="";
+        int starty = 5;//distancia das linhas
+
+
+
+        int startXEsquerda = 125;
+     
+        public class Paciente
+        {
+            public string cd_prontuario { get; set; }
+            public string nm_nome { get; set; }
+            public string dt_data_nascimento { get; set; }
+            public string nm_mae { get; set; }
+       
+           
+        }
+      /*  public class Internacao
+        {
+            public string cd_prontuario { get; set; }
+            public string nr_leito { get; set; }
+            public string dt_alta_medica { get; set; }
+            public string nm_especialidade { get; set; }
+
+        }*/
+        public Form1()
+        {
+            InitializeComponent();
+
+         
+        }
+
+
+
+        private void btImprimir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string rh = txbRh.Text;
+                //detiq = dados.getDados(be);
+                string url = "http://10.48.21.64:5003/hspmsgh-api/pacientes/paciente/" + rh;
+                WebRequest request = WebRequest.Create(url);
+                try
+                {
+                    using (var twitpicResponse = (HttpWebResponse)request.GetResponse())
+                    {
+                        using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
+                        {
+                            JsonSerializer json = new JsonSerializer();
+                            var objText = reader.ReadToEnd();
+                            paciente = JsonConvert.DeserializeObject<Paciente>(objText);
+
+                        }
+                    }
+
+                /*    string url2 = "http://10.48.21.64:5003/hspmsgh-api/internacoes/" + rh;
+                    WebRequest request2 = WebRequest.Create(url2);
+                    using (var twitpicResponse2 = (HttpWebResponse)request2.GetResponse())
+                    {
+                        using (var reader2 = new StreamReader(twitpicResponse2.GetResponseStream()))
+                        {
+                            JsonSerializer json2 = new JsonSerializer();
+                            var objText2 = reader2.ReadToEnd();
+                            internacoes = JsonConvert.DeserializeObject<List<Internacao>>(objText2);
+
+                        }
+                    }
+
+                    if (internacoes.Count != 0)
+                    {
+                        if (internacoes[0].dt_alta_medica == null)
+
+                        Andar = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(0, 2);
+                        Quarto = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(2, 2);
+                        Leito = internacoes[0].nr_leito == null ? "" : internacoes[0].nr_leito.Substring(5, 2);
+                        Clinica = internacoes[0].nm_especialidade == null ? "" : internacoes[0].nm_especialidade;
+
+
+                    }
+
+                    */
+
+                   // PrintDialog printDialog1 = new PrintDialog();
+//printDialog1.Document = printDocument1;  
+                  
+                   // printDocument1.PrinterSettings.PrinterName = "PrinterPulseira";
+
+                    printDocument1.PrinterSettings.PrinterName = "Impressora";
+                        
+                   
+                        
+                           
+                        
+                          
+                      
+                                printDocument1.Print();
+                         
+                     
+                  
+
+
+                    txbRh.Text = "";
+                  //  Andar = "";
+                  //  Quarto = "";
+                   // Leito = "";
+                   // Clinica = "";
+
+
+
+            }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Número de RH inexistente! " + ex.Message);
+                    status = 1;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Número de RH inexistente! " + ex.Message);
+                status = 1;
+
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btImprimir.Enabled = true;
+            if (status == 1)
+                lblError.Text = error;
+            else
+                lblError.ResetText();
+
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+           
+          
+            
+
+           
+
+
+
+            //DateTime dt = DateTime.Now;
+            //string data = dt.ToString("g");  
+
+            e.PageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 30, 240);//900 é a largura da página
+            //printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 500, 1000);
+           // printDialog1.PrinterSettings.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 500, 1000);
+            Graphics g = e.Graphics;
+
+
+            /*2. Print barcode...
+            using (Neodynamic.SDK.Barcode.BarcodeProfessional barcode = new Neodynamic.SDK.Barcode.BarcodeProfessional())
+            {
+                //set barcode symbology... eg Code 128
+                barcode.Symbology = Neodynamic.SDK.Barcode.Symbology.Code128;
+                //set barcode bars dimensions...
+                barcode.BarcodeUnit = Neodynamic.SDK.Barcode.BarcodeUnit.Inch;
+                barcode.BarWidth = 0.010;
+                barcode.BarHeight = 0.32;
+                //set value to encode... this time a random value...
+                barcode.Code = paciente.cd_prontuario;
+                //justify text...
+                barcode.CodeAlignment = Neodynamic.SDK.Barcode.Alignment.BelowJustify;
+                //set font for barcode human readable text...
+                using (Font fnt = new Font("Courier New", 10f))
+                {
+                    barcode.Font = fnt;
+                }
+                //print barcode below logo...
+                //IMPORTANT: barcode location unit depends on BarcodeUnit setting which in this case is INCH
+               // barcode.DrawOnCanvas(e.Graphics, new PointF(0.1f, 1.5f));
+                barcode.DrawOnCanvas(e.Graphics);
+            }*/
+
+            Image newImage = Code128Rendering.MakeBarcodeImage(paciente.cd_prontuario, 1, true);
+
+           // newImage.RotateFlip(RotateFlipType.Rotate90FlipY);
+
+            // Create coordinates for upper-left corner.
+
+           int x =5; // antes -8
+            int y = 20;
+
+            int width = 100; // com 180 funciona corretamente na maquina 
+            int height = 30; // 40 antes
+
+            // Draw image to screen.
+           
+            g.DrawImage(newImage, x, y, width, height);
+
+
+
+
+
+
+
+          g.DrawString("PRONTUARIO: " + paciente.cd_prontuario, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty);
+          g.DrawString("PACIENTE: " + paciente.nm_nome, new Font("Arial", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 15);
+          g.DrawString("NASC.: " + paciente.dt_data_nascimento, new Font("Arial", 8 , FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 30);
+            g.DrawString("MAE: " + paciente.nm_mae, new Font("Arial", 8, FontStyle.Regular), System.Drawing.Brushes.Black, startXEsquerda, starty + 45);
+
+        }
+
+        private void txbRh_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+
+                btImprimir_Click(sender, e);
+
+            }
+        }
+    }
+}
